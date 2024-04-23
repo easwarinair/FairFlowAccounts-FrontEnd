9172337ed22fb2d4fca706ae2027c1572d59ce0e
Easwari Nair <76044413+easwarinair@users.noreply.github.com>
2024-04-23 14:26:08 +0530

    Update page.js

--- src/app/blocks/page.js
+++ src/app/blocks/page.js
@@ -5,8 +5,8 @@ import "./styles.css";
 import { useRouter } from 'next/router'; 
 
 export default function Page() {
-  const router = useRouter(); 
-  const { blockId } = router.query;
+  /*const router = useRouter(); 
+  const { blockId } = router.query;*/
   return (
     <>
       <header className="header">
@@ -30,7 +30,7 @@ export default function Page() {
         <div className="rectangle-container">
           <div className="rounded-rectangle"></div>
         </div>
-        <h3 className="block-heading">Block #{blockId}</h3> 
+        <h3 className="block-heading">Block #1</h3> 
         <h4 className="block-details">Details</h4>
       </main>
     </>
