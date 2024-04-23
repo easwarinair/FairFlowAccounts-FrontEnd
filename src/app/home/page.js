755fda492daa747d25918bc5fa883f88b5a6d7a5
Easwari Nair <76044413+easwarinair@users.noreply.github.com>
2024-04-23 12:51:06 +0530

    Update page.js to display values from transaction.data

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -57,15 +57,15 @@ export default function Page() {
     return Array.from({ length: blockCount }, (_, i) => (
       <div key={i} className="transaction-details_1">
         <span>{i + 1}</span>
-        <span>Rs {Math.random() * 100000}</span>
+        <span>{transactions.i.val}</span>
         <span>
           <a href="profiles/sender.html" className="profile-link">
-            <u>@senderId</u>
+            <u>{transactions.i.sender}</u>
           </a>
         </span>
         <span>
           <a href="profiles/receiver.html" className="profile-link">
-            <u>@receiverId</u>
+            <u>{transactions.i.receiver}</u>
           </a>
         </span>
         <span>{new Date().toLocaleString()}</span>
